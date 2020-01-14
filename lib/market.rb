fec41fe1576f74aca2b786aee3dc511d801ef3b0
Mariana Cid <cidmariana5@gmail.com>
2020-01-14 11:21:46 -0700

    Added vendors the sell method

--- lib/market.rb
+++ lib/market.rb
@@ -1,7 +1,26 @@
 class Market
-  attr_reader :name, :vendors 
+  attr_reader :name, :vendors
   def initialize(name)
     @name = name
     @vendors = []
   end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map do |vendor|
+      vendor.name
+    end
+  end
+
+  def vendors_that_sell(item)
+    @vendors.find_all {|vendor| vendor.inventory.include?(item)}
+   end
+
+   def sorted_item_list
+     require "pry"; binding.pry
+     
+   end
 end
