89bfa80a571659a6595bd0357c30ef8626ab0d83
Mariana Cid <cidmariana5@gmail.com>
2020-01-14 09:15:06 -0700

    Add vendor test

---
+++ test/vendor_test.rb
@@ -0,0 +1,20 @@
+require './lib/item'
+require './lib/vendor'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class VendorTest < Minitest::Test
+
+  def test_it_exists
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_instance_of Vendor, vendor
+  end
+
+  def test_it_has_attributes
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_equal "Rocky Mountain Fresh", vendor.name
+    assert_equal ({}), vendor.inventory
+  end
+end
